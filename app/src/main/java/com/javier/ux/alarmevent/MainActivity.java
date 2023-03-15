ee60dc09babb3eb377922febd2943952d228c7b4
Carlos Javier Pita Amaya <c.pita@uniandes.edu.co>
2023-03-15 15:38:42 -0500

    Se desarrolla pantalla de registro de usuario

--- app/src/main/java/com/javier/ux/alarmevent/MainActivity.java
+++ app/src/main/java/com/javier/ux/alarmevent/MainActivity.java
@@ -1,13 +1,30 @@
 package com.javier.ux.alarmevent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegister = (Button) findViewById(R.id.idBtnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, register.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/javier/ux/alarmevent/register.java
@@ -0,0 +1,47 @@
+package com.javier.ux.alarmevent;
+
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class register extends AppCompatActivity {
+
+    Button btnCancel;
+
+    Button btnAccept;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btnCancel = (Button)findViewById(R.id.idBtnRegisterCancel);
+
+        btnAccept = (Button)findViewById(R.id.idBtnRegisterAccept);
+
+        btnCancel.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnAccept.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+              new SweetAlertDialog(register.this, SweetAlertDialog.SUCCESS_TYPE)
+                      .setTitleText("Usuario Creado con éxito")
+                      .show();
+            }
+        });
+
+
+
+    }
+}
